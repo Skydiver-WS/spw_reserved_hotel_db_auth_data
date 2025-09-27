9b41a887ccf0bb12750b20a4cd95aa93a5d899e3
Skydiver_WS <isu172@mail.ru>
2025-09-27 17:03:35 +0300

    Added in ci/cd skip test

--- src/test/java/spw_reserved_hotel_db_auth_data/SpwReservedHotelDbAuthDataApplicationTests.java
+++ src/test/java/spw_reserved_hotel_db_auth_data/SpwReservedHotelDbAuthDataApplicationTests.java
@@ -1,5 +1,6 @@
 package spw_reserved_hotel_db_auth_data;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +9,7 @@ class SpwReservedHotelDbAuthDataApplicationTests {
 
 	@Test
 	void contextLoads() {
+		Assertions.assertEquals(0, 0);
 	}
 
 }
